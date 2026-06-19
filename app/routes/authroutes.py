704bccd64f2b577be9da1640e53b7c936c6c6531
Arbin Lamsal <captainarbin@gmail.com>
2026-06-19 14:35:35 +0545

    Registered view as user route

--- app/routes/authroutes.py
+++ app/routes/authroutes.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 from app.controllers.auth import AuthController
 from app.auth import login_required, admin_required
+
+
 class AuthRoutes:
+
     def __init__(self):
         self.bp         = Blueprint("auth", __name__)
         self.controller = AuthController()
+
     def register(self):
         # ── Public pages ──────────────────────────────────────
         self.bp.route("/",        endpoint="index")(self.controller.home)
